eab3eaea62b82261fbe9d6894aa992ebca3038b0
Rohan YAdav <35034374+Rohan-07@users.noreply.github.com>
2021-06-11 01:09:29 +0530

    Update ReMedi.js

--- ethereum/ReMedi.js
+++ ethereum/ReMedi.js
@@ -3,6 +3,6 @@ import ReMedi from "./build/ReMedi.json";
 
 const instance = new web3.eth.Contract(
 	ReMedi.abi,
-	"0x79E8208d9511337f8C1c5CDeB6c9f320153eDe5b"
+	"0x79efDFf9E69dFC7A303a0e7047098156c36fEbCe"
 );
 export default instance;
